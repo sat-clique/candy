b2034880cf7c08a5072612360ef0930a4935ce3a
Felix Kutzner <felixkutzner@gmail.com>
2017-04-09 15:11:32 +0200

    generalized RSIL branching tests

--- testsrc/candy/rsil/BranchingHeuristicsTests.cc
+++ testsrc/candy/rsil/BranchingHeuristicsTests.cc
@@ -29,91 +29,150 @@
 #include <candy/testutils/TestUtils.h>
 
 namespace Candy {
+    
+    using TestedRSILBranchingHeuristic = RSILBranchingHeuristic3;
+    using TestedRSILVanishingBranchingHeuristic = RSILVanishingBranchingHeuristic3;
+    
+    template<class Heuristic>
+    static void test_uninitializedHeuristicReturnsUndefForMinInput() {
+        Heuristic underTest;
+        auto result = underTest.getAdvice({mkLit(0)}, {0}, {}, {0});
+        EXPECT_EQ(result, lit_Undef);
+    }
+    
     TEST(RSILBranchingHeuristicsTests, uninitializedHeuristicReturnsUndefForMinInput) {
-        RSILBranchingHeuristic3 underTest;
+        test_uninitializedHeuristicReturnsUndefForMinInput<TestedRSILBranchingHeuristic>();
+    }
+    
+    TEST(RSILVanishingBranchingHeuristicsTests, uninitializedHeuristicReturnsUndefForMinInput) {
+        test_uninitializedHeuristicReturnsUndefForMinInput<TestedRSILVanishingBranchingHeuristic>();
+    }
+    
+    template<class Heuristic>
+    static void test_emptyInitializedHeuristicReturnsUndefForMinInput() {
+        typename Heuristic::Parameters params{{}};
+        Heuristic underTest{params};
+        
         auto result = underTest.getAdvice({mkLit(0)}, {0}, {}, {0});
         EXPECT_EQ(result, lit_Undef);
     }
     
     TEST(RSILBranchingHeuristicsTests, emptyInitializedHeuristicReturnsUndefForMinInput) {
-        RSILBranchingHeuristic3::Parameters params{{}};
-        RSILBranchingHeuristic3 underTest{params};
-        
-        auto result = underTest.getAdvice({mkLit(0)}, {0}, {}, {0});
-        EXPECT_EQ(result, lit_Undef);
+        test_emptyInitializedHeuristicReturnsUndefForMinInput<TestedRSILBranchingHeuristic>();
     }
     
-    TEST(RSILBranchingHeuristicsTests, givesAdviceForSingleEquivalence) {
+    TEST(RSILVanishingBranchingHeuristicsTests, emptyInitializedHeuristicReturnsUndefForMinInput) {
+        test_emptyInitializedHeuristicReturnsUndefForMinInput<TestedRSILBranchingHeuristic>();
+    }
+    
+    template<class Heuristic>
+    static void test_givesAdviceForSingleEquivalence() {
         Conjectures testData;
         testData.addEquivalence(EquivalenceConjecture{{mkLit(1, 0), mkLit(2,1)}});
-        RSILBranchingHeuristic3::Parameters params{testData};
-        RSILBranchingHeuristic3 underTest{params};
+        typename Heuristic::Parameters params{testData};
+        Heuristic underTest{params};
         
-        RSILBranchingHeuristic3::TrailType testTrail {mkLit(1,0)};
-        RSILBranchingHeuristic3::TrailLimType testTrailLim {0};
-        RSILBranchingHeuristic3::DecisionType testDecisionVars {1, 1, 1, 1, 1};
-        RSILBranchingHeuristic3::AssignsType testAssigns {l_True, l_False, l_Undef, l_Undef, l_Undef};
+        typename Heuristic::TrailType testTrail {mkLit(1,0)};
+        typename Heuristic::TrailLimType testTrailLim {0};
+        typename Heuristic::DecisionType testDecisionVars {1, 1, 1, 1, 1};
+        typename Heuristic::AssignsType testAssigns {l_True, l_False, l_Undef, l_Undef, l_Undef};
         
         auto result = underTest.getAdvice(testTrail, testTrailLim, testAssigns, testDecisionVars);
         ASSERT_NE(result, lit_Undef);
         EXPECT_EQ(result, mkLit(2, 0));
     }
     
-    TEST(RSILBranchingHeuristicsTests, givesNoAdviceForSingleEquivalenceIfAssigned) {
+    TEST(RSILBranchingHeuristicsTests, givesAdviceForSingleEquivalence) {
+        test_givesAdviceForSingleEquivalence<TestedRSILBranchingHeuristic>();
+    }
+    
+    TEST(RSILVanishingBranchingHeuristicsTests, givesAdviceForSingleEquivalence) {
+        test_givesAdviceForSingleEquivalence<TestedRSILVanishingBranchingHeuristic>();
+    }
+    
+    template<class Heuristic>
+    static void test_givesNoAdviceForSingleEquivalenceIfAssigned() {
         Conjectures testData;
         testData.addEquivalence(EquivalenceConjecture{{mkLit(1, 0), mkLit(2,1)}});
-        RSILBranchingHeuristic3::Parameters params{testData};
-        RSILBranchingHeuristic3 underTest{params};
+        typename Heuristic::Parameters params{testData};
+        Heuristic underTest{params};
         
-        RSILBranchingHeuristic3::TrailType testTrail {mkLit(1,0)};
-        RSILBranchingHeuristic3::TrailLimType testTrailLim {0};
-        RSILBranchingHeuristic3::DecisionType testDecisionVars {1, 1, 1, 1, 1};
-        RSILBranchingHeuristic3::AssignsType testAssigns {l_True, l_False, l_True, l_Undef, l_Undef};
+        typename Heuristic::TrailType testTrail {mkLit(1,0)};
+        typename Heuristic::TrailLimType testTrailLim {0};
+        typename Heuristic::DecisionType testDecisionVars {1, 1, 1, 1, 1};
+        typename Heuristic::AssignsType testAssigns {l_True, l_False, l_True, l_Undef, l_Undef};
+        
+        auto result = underTest.getAdvice(testTrail, testTrailLim, testAssigns, testDecisionVars);
+        EXPECT_EQ(result, lit_Undef);
+    }
+    
+    TEST(RSILBranchingHeuristicsTests, givesNoAdviceForSingleEquivalenceIfAssigned) {
+        test_givesNoAdviceForSingleEquivalenceIfAssigned<TestedRSILBranchingHeuristic>();
+    }
+    
+    TEST(RSILVanishingBranchingHeuristicsTests, givesNoAdviceForSingleEquivalenceIfAssigned) {
+        test_givesNoAdviceForSingleEquivalenceIfAssigned<TestedRSILVanishingBranchingHeuristic>();
+    }
+    
+    template<class Heuristic>
+    static void test_givesNoAdviceForSingleEquivalenceIfNotEligibleForDecision() {
+        Conjectures testData;
+        testData.addEquivalence(EquivalenceConjecture{{mkLit(1, 0), mkLit(2,1)}});
+        typename Heuristic::Parameters params{testData};
+        Heuristic underTest{params};
+        
+        typename Heuristic::TrailType testTrail {mkLit(1,0)};
+        typename Heuristic::TrailLimType testTrailLim {0};
+        typename Heuristic::DecisionType testDecisionVars {1, 1, 0, 1, 1};
+        typename Heuristic::AssignsType testAssigns {l_True, l_False, l_True, l_Undef, l_Undef};
         
         auto result = underTest.getAdvice(testTrail, testTrailLim, testAssigns, testDecisionVars);
         EXPECT_EQ(result, lit_Undef);
     }
     
     TEST(RSILBranchingHeuristicsTests, givesNoAdviceForSingleEquivalenceIfNotEligibleForDecision) {
+        test_givesNoAdviceForSingleEquivalenceIfNotEligibleForDecision<TestedRSILBranchingHeuristic>();
+    }
+    
+    TEST(RSILVanishingBranchingHeuristicsTests, givesNoAdviceForSingleEquivalenceIfNotEligibleForDecision) {
+        test_givesNoAdviceForSingleEquivalenceIfNotEligibleForDecision<TestedRSILVanishingBranchingHeuristic>();
+    }
+    
+    template<class Heuristic>
+    static void test_givesNoAdviceForSingleEquivalenceIfIrrelevant() {
         Conjectures testData;
         testData.addEquivalence(EquivalenceConjecture{{mkLit(1, 0), mkLit(2,1)}});
-        RSILBranchingHeuristic3::Parameters params{testData};
-        RSILBranchingHeuristic3 underTest{params};
+        typename Heuristic::Parameters params{testData};
+        Heuristic underTest{params};
         
-        RSILBranchingHeuristic3::TrailType testTrail {mkLit(1,0)};
-        RSILBranchingHeuristic3::TrailLimType testTrailLim {0};
-        RSILBranchingHeuristic3::DecisionType testDecisionVars {1, 1, 0, 1, 1};
-        RSILBranchingHeuristic3::AssignsType testAssigns {l_True, l_False, l_True, l_Undef, l_Undef};
+        typename Heuristic::TrailType testTrail {mkLit(4,0)};
+        typename Heuristic::TrailLimType testTrailLim {0};
+        typename Heuristic::DecisionType testDecisionVars {1, 1, 1, 1, 1};
+        typename Heuristic::AssignsType testAssigns {l_True, l_True, l_Undef, l_Undef, l_False};
         
         auto result = underTest.getAdvice(testTrail, testTrailLim, testAssigns, testDecisionVars);
         EXPECT_EQ(result, lit_Undef);
     }
     
     TEST(RSILBranchingHeuristicsTests, givesNoAdviceForSingleEquivalenceIfIrrelevant) {
-        Conjectures testData;
-        testData.addEquivalence(EquivalenceConjecture{{mkLit(1, 0), mkLit(2,1)}});
-        RSILBranchingHeuristic3::Parameters params{testData};
-        RSILBranchingHeuristic3 underTest{params};
-        
-        RSILBranchingHeuristic3::TrailType testTrail {mkLit(4,0)};
-        RSILBranchingHeuristic3::TrailLimType testTrailLim {0};
-        RSILBranchingHeuristic3::DecisionType testDecisionVars {1, 1, 1, 1, 1};
-        RSILBranchingHeuristic3::AssignsType testAssigns {l_True, l_True, l_Undef, l_Undef, l_False};
-        
-        auto result = underTest.getAdvice(testTrail, testTrailLim, testAssigns, testDecisionVars);
-        EXPECT_EQ(result, lit_Undef);
+        test_givesNoAdviceForSingleEquivalenceIfIrrelevant<TestedRSILBranchingHeuristic>();
     }
     
-    TEST(RSILBranchingHeuristicsTests, givesAdviceForSingleEquivalenceSize3) {
+    TEST(RSILVanishingBranchingHeuristicsTests, givesNoAdviceForSingleEquivalenceIfIrrelevant) {
+        test_givesNoAdviceForSingleEquivalenceIfIrrelevant<TestedRSILVanishingBranchingHeuristic>();
+    }
+    
+    template<class Heuristic>
+    static void test_givesAdviceForSingleEquivalenceSize3() {
         Conjectures testData;
         testData.addEquivalence(EquivalenceConjecture{{mkLit(1, 0), mkLit(3,1), mkLit(2, 0)}});
-        RSILBranchingHeuristic3::Parameters params{testData};
-        RSILBranchingHeuristic3 underTest{params};
+        typename Heuristic::Parameters params{testData};
+        Heuristic underTest{params};
         
-        RSILBranchingHeuristic3::TrailType testTrail {mkLit(3,0)};
-        RSILBranchingHeuristic3::TrailLimType testTrailLim {0};
-        RSILBranchingHeuristic3::DecisionType testDecisionVars {1, 1, 1, 1, 1};
-        RSILBranchingHeuristic3::AssignsType testAssigns {l_Undef, l_Undef, l_Undef, l_False, l_Undef};
+        typename Heuristic::TrailType testTrail {mkLit(3,0)};
+        typename Heuristic::TrailLimType testTrailLim {0};
+        typename Heuristic::DecisionType testDecisionVars {1, 1, 1, 1, 1};
+        typename Heuristic::AssignsType testAssigns {l_Undef, l_Undef, l_Undef, l_False, l_Undef};
         
         auto result = underTest.getAdvice(testTrail, testTrailLim, testAssigns, testDecisionVars);
         ASSERT_NE(result, lit_Undef);
@@ -121,16 +180,24 @@ namespace Candy {
         EXPECT_TRUE((result == mkLit(1, 0)) || (result == mkLit(2,0)));
     }
     
+    TEST(RSILBranchingHeuristicsTests, givesAdviceForSingleEquivalenceSize3) {
+        test_givesAdviceForSingleEquivalenceSize3<TestedRSILBranchingHeuristic>();
+    }
+    
+    TEST(RSILVanishingBranchingHeuristicsTests, givesAdviceForSingleEquivalenceSize3) {
+        test_givesAdviceForSingleEquivalenceSize3<TestedRSILVanishingBranchingHeuristic>();
+    }
+    
     TEST(RSILBranchingHeuristicsTests, travelsUpTrail) {
         Conjectures testData;
         testData.addEquivalence(EquivalenceConjecture{{mkLit(1, 0), mkLit(3,1), mkLit(2, 0)}});
-        RSILBranchingHeuristic3::Parameters params{testData};
-        RSILBranchingHeuristic3 underTest{params};
+        TestedRSILBranchingHeuristic::Parameters params{testData};
+        TestedRSILBranchingHeuristic underTest{params};
         
-        RSILBranchingHeuristic3::TrailType testTrail {mkLit(3,0), mkLit(4,0)};
-        RSILBranchingHeuristic3::TrailLimType testTrailLim {0};
-        RSILBranchingHeuristic3::DecisionType testDecisionVars {1, 1, 1, 1, 1};
-        RSILBranchingHeuristic3::AssignsType testAssigns {l_Undef, l_Undef, l_Undef, l_False, l_False};
+        TestedRSILBranchingHeuristic::TrailType testTrail {mkLit(3,0), mkLit(4,0)};
+        TestedRSILBranchingHeuristic::TrailLimType testTrailLim {0};
+        TestedRSILBranchingHeuristic::DecisionType testDecisionVars {1, 1, 1, 1, 1};
+        TestedRSILBranchingHeuristic::AssignsType testAssigns {l_Undef, l_Undef, l_Undef, l_False, l_False};
         
         auto result = underTest.getAdvice(testTrail, testTrailLim, testAssigns, testDecisionVars);
         ASSERT_NE(result, lit_Undef);
@@ -142,14 +209,14 @@ namespace Candy {
         Conjectures testData;
         
         testData.addEquivalence(EquivalenceConjecture{{mkLit(1, 0), mkLit(2,1)}});
-        RSILVanishingBranchingHeuristic3::Parameters params{{testData}};
+        TestedRSILVanishingBranchingHeuristic::Parameters params{{testData}};
         params.probHalfLife = 100ull;
-        RSILVanishingBranchingHeuristic3 underTest{params};
+        TestedRSILVanishingBranchingHeuristic underTest{params};
         
-        RSILBranchingHeuristic3::TrailType testTrail {mkLit(1,0)};
-        RSILBranchingHeuristic3::TrailLimType testTrailLim {0};
-        RSILBranchingHeuristic3::DecisionType testDecisionVars {1, 1, 1, 1, 1};
-        RSILBranchingHeuristic3::AssignsType testAssigns {l_True, l_False, l_Undef, l_Undef, l_Undef};
+        TestedRSILBranchingHeuristic::TrailType testTrail {mkLit(1,0)};
+        TestedRSILBranchingHeuristic::TrailLimType testTrailLim {0};
+        TestedRSILBranchingHeuristic::DecisionType testDecisionVars {1, 1, 1, 1, 1};
+        TestedRSILBranchingHeuristic::AssignsType testAssigns {l_True, l_False, l_Undef, l_Undef, l_Undef};
         
         int calls = 0;
         for (int i = 0; i < 100; ++i) {
@@ -166,14 +233,14 @@ namespace Candy {
         const int stages = 20;
         
         testData.addEquivalence(EquivalenceConjecture{{mkLit(1, 0), mkLit(2,1)}});
-        RSILVanishingBranchingHeuristic3::Parameters params{{testData}};
+        TestedRSILVanishingBranchingHeuristic::Parameters params{{testData}};
         params.probHalfLife = halfLife;
-        RSILVanishingBranchingHeuristic3 underTest{params};
+        TestedRSILVanishingBranchingHeuristic underTest{params};
         
-        RSILBranchingHeuristic3::TrailType testTrail {mkLit(1,0)};
-        RSILBranchingHeuristic3::TrailLimType testTrailLim {0};
-        RSILBranchingHeuristic3::DecisionType testDecisionVars {1, 1, 1, 1, 1};
-        RSILBranchingHeuristic3::AssignsType testAssigns {l_True, l_False, l_Undef, l_Undef, l_Undef};
+        TestedRSILBranchingHeuristic::TrailType testTrail {mkLit(1,0)};
+        TestedRSILBranchingHeuristic::TrailLimType testTrailLim {0};
+        TestedRSILBranchingHeuristic::DecisionType testDecisionVars {1, 1, 1, 1, 1};
+        TestedRSILBranchingHeuristic::AssignsType testAssigns {l_True, l_False, l_Undef, l_Undef, l_Undef};
         
         std::unordered_map<uint8_t, double> distribution;
         std::unordered_map<uint8_t, double> referenceDistribution;
