a9399dd856cc925f12c61803517bf017ca49130e
Felix Kutzner <felixkutzner@gmail.com>
2017-05-27 17:39:33 +0200

    added negative unit tests for MiterDetector

--- testsrc/candy/gates/MiterDetectorTests.cc
+++ testsrc/candy/gates/MiterDetectorTests.cc
@@ -26,4 +26,70 @@
 
 #include <gtest/gtest.h>
 
+#include <candy/gates/GateAnalyzer.h>
 #include <candy/gates/MiterDetector.h>
+#include <candy/testutils/TestGateStructure.h>
+#include <candy/core/SolverTypes.h>
+
+namespace Candy {
+    TEST(GateMiterDetectorTests, rejectsMonotonousStructure) {
+        auto gateBuilder = createGateStructureBuilder();
+        gateBuilder->withAnd({mkLit(1, 1), mkLit(2, 1)}, mkLit(0,1));
+        gateBuilder->withAnd({mkLit(3, 1), mkLit(4, 1)}, mkLit(1,1));
+        gateBuilder->withAnd({mkLit(3, 1), mkLit(5, 1)}, mkLit(2,1));
+        gateBuilder->withXor({mkLit(6, 1), mkLit(7,1)}, mkLit(3,1));
+        gateBuilder->withXor({mkLit(7, 1), mkLit(8,1)}, mkLit(4,1));
+        gateBuilder->withXor({mkLit(9, 1), mkLit(10,1)}, mkLit(5,1));
+
+        auto problem = gateBuilder->build();
+        
+        GateAnalyzer analyzer(*problem);
+        analyzer.analyze();
+        ASSERT_EQ(analyzer.getGateCount(), 6);
+        EXPECT_FALSE(hasPossiblyMiterStructure(analyzer));
+    }
+    
+    TEST(GateMiterDetectorTests, rejectsEmptyStructure) {
+        auto gateBuilder = createGateStructureBuilder();
+        auto problem = gateBuilder->build();
+        
+        GateAnalyzer analyzer(*problem);
+        analyzer.analyze();
+        ASSERT_EQ(analyzer.getGateCount(), 0);
+        EXPECT_FALSE(hasPossiblyMiterStructure(analyzer));
+    }
+    
+    TEST(GateMiterDetectorTests, rejectsNearMiter) {
+        auto gateBuilder = createGateStructureBuilder();
+        // miter "base" structure
+        gateBuilder->withOr({mkLit(1, 1), mkLit(2, 1)}, mkLit(0, 1));
+        gateBuilder->withOr({mkLit(3, 1), mkLit(4, 1)}, mkLit(1, 1));
+        gateBuilder->withOr({mkLit(5, 1), mkLit(6, 1)}, mkLit(2, 1));
+        gateBuilder->withXor({mkLit(7, 1), mkLit(8, 1)}, mkLit(3, 1));
+        gateBuilder->withXor({mkLit(9, 1), mkLit(10, 1)}, mkLit(4, 1));
+        gateBuilder->withXor({mkLit(11, 1), mkLit(12,1)}, mkLit(5, 1));
+        gateBuilder->withAnd({mkLit(13, 1), mkLit(14,1)}, mkLit(6, 1));
+        
+        // circuit 1
+        gateBuilder->withAnd({mkLit(15, 1), mkLit(16, 1)}, mkLit(7, 1));
+        gateBuilder->withAnd({mkLit(15, 1), mkLit(16, 1)}, mkLit(8, 1));
+        
+        // circuit 2
+        gateBuilder->withOr({mkLit(17, 1), mkLit(18, 1)}, mkLit(9, 0));
+        gateBuilder->withOr({mkLit(17, 1), mkLit(18, 1)}, mkLit(10, 0));
+        
+        // circuit 3
+        gateBuilder->withXor({mkLit(17, 1), mkLit(18, 1)}, mkLit(11, 0));
+        gateBuilder->withXor({mkLit(17, 1), mkLit(18, 1)}, mkLit(12, 0));
+        
+        // circuit 4 (var 14 remains input, "single-line circuit" case)
+        gateBuilder->withXor({mkLit(17, 1), mkLit(18, 1)}, mkLit(13, 0));
+        
+        auto problem = gateBuilder->build();
+        
+        GateAnalyzer analyzer(*problem);
+        analyzer.analyze();
+        ASSERT_EQ(analyzer.getGateCount(), 14);
+        EXPECT_FALSE(hasPossiblyMiterStructure(analyzer));
+    }
+}
