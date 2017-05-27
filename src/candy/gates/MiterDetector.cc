7220f830585081ead1f479010fff880aebdfd786
Felix Kutzner <felixkutzner@gmail.com>
2017-05-27 20:33:31 +0200

    added a simple implementation of MiterDetector

--- src/candy/gates/MiterDetector.cc
+++ src/candy/gates/MiterDetector.cc
@@ -26,9 +26,230 @@
 
 #include "MiterDetector.h"
 #include <candy/gates/GateAnalyzer.h>
+#include <candy/gates/GateDFSTraversal.h>
+#include <candy/core/SolverTypes.h>
+
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
 
 namespace Candy {
+    namespace {
+        
+        /**
+         * \ingroup Gates
+         *
+         * \brief collects the monotonously nested "border" gates at the edge of the
+         *   monotonously nested region of the gate structure.
+         */
+        class MonotonousRegionBorderGateCollector {
+        public:
+            
+            void backtrack(Gate* g) {
+                bool isBorderGate = true;
+                for (Lit input : g->getInputs()) {
+                    Var inputVar = var(input);
+                    isBorderGate &= m_seenOuts.find(inputVar) == m_seenOuts.end();
+                }
+                
+                if (isBorderGate) {
+                    m_monotonousRegionBorder.insert(g);
+                }
+            }
+            
+            void collect(Gate* g) {
+                m_seenOuts.insert(var(g->getOutput()));
+            }
+            
+            void collectInput(Var var) const {
+                (void) var;
+            }
+            
+            void init(size_t n) const {
+                (void) n;
+            }
+            
+            bool pruneAt(Gate& g) {
+                // The nonmonotonously nested part of the gate structure is not taken
+                // into account by this heuristic
+                m_seenNonmonotonous |= g.hasNonMonotonousParent();
+                return g.hasNonMonotonousParent();
+            }
+            
+            void finished() {
+                m_seenOuts.clear();
+            }
+            
+            const std::unordered_set<Gate*>& getMonotonousRegionBorder() const {
+                return m_monotonousRegionBorder;
+            }
+            
+            bool hasSeenNonmonotonouslyNestedGate() {
+                return m_seenNonmonotonous;
+            }
+            
+        private:
+            std::unordered_set<Var> m_seenOuts;
+            std::unordered_set<Gate*> m_monotonousRegionBorder;
+            bool m_seenNonmonotonous = false;
+        };
+        
+        /**
+         * \ingroup Gates
+         *
+         * \brief checks if all given gates have exactly two input variables
+         *
+         * \returns true iff G only contains gates with exactly two input variables.
+         */
+        template<typename CollectionType>
+        bool allGatesHaveTwoInputs(CollectionType& gates) {
+            std::unordered_set<Var> vars;
+            
+            for(Gate* g : gates) {
+                vars.clear();
+                for (auto input : g->getInputs()) {
+                    vars.insert(var(input));
+                }
+                if (vars.size() != 2) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        class SignOccurence {
+        public:
+            void setOccured(bool sign) {
+                if (sign) {
+                    m_positive = true;
+                }
+                else {
+                    m_negative = true;
+                }
+            }
+            
+            bool hasOccured(bool sign) {
+                return sign ? m_positive : m_negative;
+            }
+            
+            bool isSaturated() {
+                return m_positive && m_negative;
+            }
+            
+        private:
+            bool m_positive = false;
+            bool m_negative = false;
+        };
+        
+        /**
+         * \ingroup Gates
+         *
+         * \brief checks whether all given gates gates are potentially the two
+         *    input-receiving gates of an AIG-encoded binary XOR.
+         *
+         * \param gates     A collection G of gates, each with exactly 2 input variables.
+         *
+         * \returns         true iff G is a collection of gates as specified.
+         */
+        template<typename CollectionType>
+        bool allGatesAreAIGXORsWithUniqueInputVars(CollectionType& gates) {
+            std::unordered_map<Var, SignOccurence> literalOccurences;
+            
+            for (Gate* gate : gates) {
+                for (Lit input : gate->getInputs()) {
+                    Var inpVar = var(input);
+                    bool inpSign = sign(input);
+                    
+                    if (literalOccurences[inpVar].hasOccured(inpSign)) {
+                        return false;
+                    }
+                    literalOccurences[inpVar].setOccured(inpSign);
+                }
+            }
+            
+            for (auto kv : literalOccurences) {
+                if (!kv.second.isSaturated()) {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+    
+        /**
+         * \ingroup Gates
+         *
+         * \brief checks whether the given gate is a binary XOR gate.
+         *
+         * \param g     An arbitrary gate.
+         *
+         * \returns     true iff g is a binary XOR gate.
+         */
+        bool isBinaryXORGate(Gate& g) {
+            auto& clauses = g.getForwardClauses();
+            if (clauses.size() != 2ull) {
+                return false;
+            }
+            auto& clause1 = *(clauses[0]);
+            auto& clause2 = *(clauses[1]);
+            
+            if (clause1.size() != 3ull || clause2.size() != 3ull) {
+                return false;
+            }
+            
+            // TODO: optimize allocations by reusing gateLits
+            std::unordered_set<Lit> gateLits;
+            gateLits.insert(clause1.begin(), clause1.end());
+            gateLits.insert(clause2.begin(), clause2.end());
+            return gateLits.size() == 5ull;
+        }
+        
+        /**
+         * \ingroup Gates
+         *
+         * \brief checks whether the given gates are binary XOR gates with
+         *   unique input variables (wrt. the other given gates)
+         *
+         * \param g     A collection of gates G.
+         *
+         * \returns true    iff G is a collection of binary XOR gates as described.
+         */
+        template<typename CollectionType>
+        bool allGatesAreBinaryXORsWithUniqueInputVars(CollectionType& gates) {
+            std::unordered_set<Var> seenInputVars;
+            
+            for (Gate* gate : gates) {
+                if (!isBinaryXORGate(*gate)) {
+                    return false;
+                }
+                
+                for(Lit input : gate->getInputs()) {
+                    if (seenInputVars.find(var(input)) != seenInputVars.end()) {
+                        return false;
+                    }
+                }
+                
+                for(Lit input : gate->getInputs()) {
+                    seenInputVars.insert(var(input));
+                }
+            }
+            
+            return true;
+        }
+    }
+    
     bool hasPossiblyMiterStructure(GateAnalyzer& analyzer) {
-        return false;
+        auto borderGateCollector = traverseDFS<MonotonousRegionBorderGateCollector>(analyzer);
+        auto& borderGates = borderGateCollector.getMonotonousRegionBorder();
+        
+        // Some quick preliminary checks to quickly rule out non-miters
+        if (borderGates.empty()
+            || !borderGateCollector.hasSeenNonmonotonouslyNestedGate()
+            || !allGatesHaveTwoInputs(borderGates)) {
+            return false;
+        }
+        
+        return  (allGatesAreAIGXORsWithUniqueInputVars(borderGates)
+                 || allGatesAreBinaryXORsWithUniqueInputVars(borderGates));
     }
 }
--- testsrc/candy/gates/MiterDetectorTests.cc
+++ testsrc/candy/gates/MiterDetectorTests.cc
@@ -32,18 +32,35 @@
 #include <candy/core/SolverTypes.h>
 
 namespace Candy {
-    TEST(GateMiterDetectorTests, rejectsMonotonousStructure) {
+    TEST(GateMiterDetectorTests, rejectsMonotonousNonmiterStructure) {
         auto gateBuilder = createGateStructureBuilder();
-        gateBuilder->withAnd({mkLit(1, 1), mkLit(2, 1)}, mkLit(0,1));
-        gateBuilder->withAnd({mkLit(3, 1), mkLit(4, 1)}, mkLit(1,1));
-        gateBuilder->withAnd({mkLit(3, 1), mkLit(5, 1)}, mkLit(2,1));
-        gateBuilder->withXor({mkLit(6, 1), mkLit(7,1)}, mkLit(3,1));
-        gateBuilder->withXor({mkLit(7, 1), mkLit(8,1)}, mkLit(4,1));
-        gateBuilder->withXor({mkLit(9, 1), mkLit(10,1)}, mkLit(5,1));
+        gateBuilder->withAnd({mkLit(1, 1), mkLit(2, 1)}, mkLit(0, 1));
+        gateBuilder->withAnd({mkLit(3, 1), mkLit(4, 1)}, mkLit(1, 1));
+        gateBuilder->withAnd({mkLit(3, 1), mkLit(5, 1)}, mkLit(2, 1));
+        gateBuilder->withXor({mkLit(6, 1), mkLit(7, 1)}, mkLit(3, 1));
+        gateBuilder->withXor({mkLit(7, 1), mkLit(8, 1)}, mkLit(4, 1));
+        gateBuilder->withXor({mkLit(9, 1), mkLit(10, 1)}, mkLit(5, 1));
 
         auto problem = gateBuilder->build();
         
-        GateAnalyzer analyzer(*problem);
+        GateAnalyzer analyzer {*problem};
+        analyzer.analyze();
+        ASSERT_EQ(analyzer.getGateCount(), 6);
+        EXPECT_FALSE(hasPossiblyMiterStructure(analyzer));
+    }
+    
+    TEST(GateMiterDetectorTests, rejectsMonotonousMiterStructure) {
+        auto gateBuilder = createGateStructureBuilder();
+        gateBuilder->withAnd({mkLit(1, 1), mkLit(2, 1)}, mkLit(0, 1));
+        gateBuilder->withAnd({mkLit(3, 1), mkLit(4, 1)}, mkLit(1, 1));
+        gateBuilder->withAnd({mkLit(3, 1), mkLit(5, 1)}, mkLit(2, 1));
+        gateBuilder->withXor({mkLit(6, 1), mkLit(7, 1)}, mkLit(3, 1));
+        gateBuilder->withXor({mkLit(8, 1), mkLit(9, 1)}, mkLit(4, 1));
+        gateBuilder->withXor({mkLit(10, 1), mkLit(11, 1)}, mkLit(5, 1));
+        
+        auto problem = gateBuilder->build();
+        
+        GateAnalyzer analyzer {*problem};
         analyzer.analyze();
         ASSERT_EQ(analyzer.getGateCount(), 6);
         EXPECT_FALSE(hasPossiblyMiterStructure(analyzer));
@@ -53,7 +70,7 @@ namespace Candy {
         auto gateBuilder = createGateStructureBuilder();
         auto problem = gateBuilder->build();
         
-        GateAnalyzer analyzer(*problem);
+        GateAnalyzer analyzer {*problem};
         analyzer.analyze();
         ASSERT_EQ(analyzer.getGateCount(), 0);
         EXPECT_FALSE(hasPossiblyMiterStructure(analyzer));
@@ -82,14 +99,111 @@ namespace Candy {
         gateBuilder->withXor({mkLit(17, 1), mkLit(18, 1)}, mkLit(11, 0));
         gateBuilder->withXor({mkLit(17, 1), mkLit(18, 1)}, mkLit(12, 0));
         
+        auto problem = gateBuilder->build();
+        
+        GateAnalyzer analyzer {*problem};
+        analyzer.analyze();
+        ASSERT_EQ(analyzer.getGateCount(), 13);
+        EXPECT_FALSE(hasPossiblyMiterStructure(analyzer));
+    }
+    
+    TEST(GateMiterDetectorTests, acceptsNonAIGMiter) {
+        auto gateBuilder = createGateStructureBuilder();
+        // miter "base" structure
+        gateBuilder->withOr({mkLit(1, 1), mkLit(2, 1)}, mkLit(0, 1));
+        gateBuilder->withOr({mkLit(3, 1), mkLit(4, 1)}, mkLit(1, 1));
+        gateBuilder->withOr({mkLit(5, 1), mkLit(6, 1)}, mkLit(2, 1));
+        gateBuilder->withXor({mkLit(7, 1), mkLit(8, 1)}, mkLit(3, 1));
+        gateBuilder->withXor({mkLit(9, 1), mkLit(10, 1)}, mkLit(4, 1));
+        gateBuilder->withXor({mkLit(11, 1), mkLit(12,1)}, mkLit(5, 1));
+        gateBuilder->withXor({mkLit(13, 1), mkLit(14,1)}, mkLit(6, 1));
+        
+        // circuit 1
+        gateBuilder->withAnd({mkLit(15, 1), mkLit(16, 1)}, mkLit(7, 1));
+        gateBuilder->withAnd({mkLit(15, 1), mkLit(16, 1)}, mkLit(8, 1));
+        
+        // circuit 2
+        gateBuilder->withOr({mkLit(17, 1), mkLit(18, 1)}, mkLit(9, 0));
+        gateBuilder->withOr({mkLit(17, 1), mkLit(18, 1)}, mkLit(10, 0));
+        
+        // circuit 3
+        gateBuilder->withXor({mkLit(17, 1), mkLit(18, 1)}, mkLit(11, 0));
+        gateBuilder->withXor({mkLit(17, 1), mkLit(18, 1)}, mkLit(12, 0));
+        
         // circuit 4 (var 14 remains input, "single-line circuit" case)
         gateBuilder->withXor({mkLit(17, 1), mkLit(18, 1)}, mkLit(13, 0));
         
         auto problem = gateBuilder->build();
         
-        GateAnalyzer analyzer(*problem);
+        GateAnalyzer analyzer {*problem};
         analyzer.analyze();
         ASSERT_EQ(analyzer.getGateCount(), 14);
-        EXPECT_FALSE(hasPossiblyMiterStructure(analyzer));
+        EXPECT_TRUE(hasPossiblyMiterStructure(analyzer));
+    }
+    
+    namespace {
+        void addAIGXOR(GateStructureBuilder& gateBuilder,
+                       Lit input1, Lit input2,
+                       Lit intermediate1, Lit intermediate2,
+                       Lit output) {
+            gateBuilder.withAnd({~intermediate1, ~intermediate2}, output);
+            gateBuilder.withAnd({input1, ~input2}, intermediate1);
+            gateBuilder.withAnd({~input1, input2}, intermediate2);
+        }
+    }
+    
+    TEST(GateMiterDetectorTests, acceptsAIGMiter) {
+        auto gateBuilder = createGateStructureBuilder();
+        // miter "base" structure
+        gateBuilder->withOr({mkLit(1, 1), mkLit(2, 1)}, mkLit(0, 1));
+        gateBuilder->withOr({mkLit(3, 1), mkLit(4, 1)}, mkLit(1, 1));
+        
+        addAIGXOR(*gateBuilder,
+                  mkLit(7, 1), mkLit(8, 1),
+                  mkLit(9, 1), mkLit(10, 1),
+                  mkLit(2, 1));
+        
+        addAIGXOR(*gateBuilder,
+                  mkLit(11, 1), mkLit(12, 1),
+                  mkLit(13, 1), mkLit(14, 1),
+                  mkLit(3, 1));
+        
+        addAIGXOR(*gateBuilder,
+                  mkLit(15, 1), mkLit(16, 1),
+                  mkLit(17, 1), mkLit(18, 1),
+                  mkLit(4, 1));
+        
+        // add at least one nonmonotonously nested gate
+        gateBuilder->withAnd({mkLit(19, 1), mkLit(20, 1)}, mkLit(15, 1));
+        gateBuilder->withAnd({mkLit(19, 1), mkLit(20, 1)}, mkLit(16, 1));
+        
+        auto problem = gateBuilder->build();
+        
+        GateAnalyzer analyzer {*problem};
+        analyzer.analyze();
+        ASSERT_EQ(analyzer.getGateCount(), 13);
+        EXPECT_TRUE(hasPossiblyMiterStructure(analyzer));
+    }
+    
+    namespace {
+        void miterdetectionAcceptanceTest(const std::string& filename, bool expectedResult) {
+            std::string fullFilename = "problems/" + filename + ".cnf";
+            
+            CNFProblem problem;
+            problem.readDimacsFromFile(fullFilename.c_str());
+            ASSERT_FALSE(problem.getProblem().empty()) << "Could not read test problem file.";
+            
+            GateAnalyzer analyzer{problem};
+            analyzer.analyze();
+            EXPECT_EQ(hasPossiblyMiterStructure(analyzer), expectedResult);
+        }
+    }
+    
+    TEST(GateMiterDetectorTests, acceptance_acceptsAIGMiter_6s33) {
+        miterdetectionAcceptanceTest("6s33", true);
+    }
+    
+    TEST(GateMiterDetectorTests, acceptance_rejectsNonMiter_flat200_1) {
+        miterdetectionAcceptanceTest("flat200-1", false);
     }
 }
