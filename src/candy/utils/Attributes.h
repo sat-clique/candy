b0c4ec565a18bc28c341ad21002713e10a333615
Markus Iser <2.markus.iser@gmail.com>
2017-06-14 13:12:29 +0200

    provide *likely and assume macros

--- src/candy/utils/Attributes.h
+++ src/candy/utils/Attributes.h
@@ -5,11 +5,17 @@
   #define ATTR_ALWAYSINLINE __attribute__((always_inline))
   #define ATTR_ALIGNED(x) __attribute__((__aligned__(x)))
   #define ATTR_RESTRICT __restrict__
+  #define ASSUME(cond) do { if (!(cond)) __builtin_unreachable(); } while (0)
+  #define LIKELY(expr) __builtin_expect(!(expr), 0)
+  #define UNLIKELY(expr) __builtin_expect((expr),  0)
 #elif defined(_MSC_VER)
   // TODO
   #define ATTR_ALWAYSINLINE
   #define ATTR_ALIGNED(x)
   #define ATTR_RESTRICT
+  #define ASSUME(cond) __assume(cond);
+  #define LIKELY(expr)
+  #define UNLIKELY(expr)
 #endif
 
 #endif
