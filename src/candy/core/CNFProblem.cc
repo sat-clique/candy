9af9522992f78d5541c99f3b06386a55d4361575
Markus Iser <2.markus.iser@gmail.com>
2018-10-31 18:46:44 +0100

    fix new crash in case of empty problem

--- src/candy/core/CNFProblem.cc
+++ src/candy/core/CNFProblem.cc
@@ -14,15 +14,19 @@ namespace Candy {
 
 std::vector<double> CNFProblem::getLiteralRelativeOccurrences() const {
     std::vector<double> literalOccurrence(maxVars*2, 0.0);
-    for (auto c : problem) {
-        for (Lit lit : *c) {
-            literalOccurrence[lit] += 1.0 / c->size();
+
+    if (nVars() > 0) {
+        for (auto c : problem) {
+            for (Lit lit : *c) {
+                literalOccurrence[lit] += 1.0 / c->size();
+            }
+        }
+        double max = *std::max_element(literalOccurrence.begin(), literalOccurrence.end());
+        for (double& occ : literalOccurrence) {
+            occ = occ / max;
         }
     }
-    double max = *std::max_element(literalOccurrence.begin(), literalOccurrence.end());
-    for (double& occ : literalOccurrence) {
-        occ = occ / max;
-    }
+    
     return literalOccurrence;
 }
 
