9130417f859d93c70b38c712d82c6caa234a1ba0
Markus Iser <2.markus.iser@gmail.com>
2019-03-06 16:10:13 +0100

    stop startin further threads if result is already found

--- src/candy/Main.cc
+++ src/candy/Main.cc
@@ -220,7 +220,7 @@ int main(int argc, char** argv) {
         installSignalHandlers(false);
     }
     else {
-        for (unsigned int count = 0; count < (unsigned int)ParallelOptions::opt_threads; count++) {
+        for (unsigned int count = 0; count < (unsigned int)ParallelOptions::opt_threads && result == l_Undef; count++) {
             std::cout << "c Initializing Solver " << count << std::endl;
             ClauseDatabaseOptions::opt_recalculate_lbd = false;
             SolverOptions::opt_sort_watches = ((count % 2) == 0);
