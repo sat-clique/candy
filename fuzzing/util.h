0012369b79ca2c8f483d7e39941a8920b8753823
Markus Iser <markus.iser@kit.edu>
2020-04-16 12:07:28 +0200

    removed fuzzing directory.. for real

--- fuzzing/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef _FUZZING_UTIL_H
-#define _FUZZING_UTIL_H
-
-#include "candy/core/SolverTypes.h"
-#include "candy/core/CNFProblem.h"
-
-extern Candy::lbool minisat_result(Candy::CNFProblem const &problem);
-
-#endif
